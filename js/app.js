88a7ed02b530d3c14b17998e545dcbf980ed1a04
highlydubious <alexdubelko@gmail.com>
2014-08-25 16:23:18 -0700

    Before potential disaster

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
-var planetApp = angular.module('planetApp', []);
+var tttApp = angular.module('tttApp', ["firebase"]);
 
-planetApp.controller('tttCtrl', function ($scope) {
+tttApp.controller('tttCtrl', ['$scope', '$firebase', function ($scope, $firebase) {
 
 	$scope.cells = ["","","","","","","","",""];
+	$scope.cells2 = {cells:["X","O","O","X","","","","",""]};
+	var ref = $firebase( new Firebase("https://tictacking.firebaseio.com/data"));
+	ref.$bind($scope, "cells2");
+
+	$scope.$watch('cells2', function() {
+		console.log ("hello");
+	});
+
 
 	$scope.turn = 0;
 
@@ -20,30 +28,28 @@ planetApp.controller('tttCtrl', function ($scope) {
 			}
 			else 
 			{
-				$scope.cells[$index] = 'O';
+				$scope.cells[$index] = '0';
 			}
 
 		} 
 	}
 
 
-
-
-
   
-});
+}]);
 
 
 ///  Win Condition ///
 
+// $scope.winChecker = function () 
+// {
+// 	if ( ($scope.cells[0] && $scope.cells[1] && $scope.cells[2] === 'X') || $scope.cells[3] && $scope.cells[4] && $scope.cells[5] === 'X' )
+// 			{
+// 				console.log('winner!');
+// 			}
+// }
+//  var array = []
 
-
-$scope.winChecker = function () {
-	if ( ($scope.cells[0] && $scope.cells[1] && $scope.cells[2] === 'X') || $scope.cells[3] && $scope.cells[4] && $scope.cells[5] === 'X' )
-			{
-				console.log('winner!');
-			}
-}
 
 
 
